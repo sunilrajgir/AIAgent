6ed70632a2cf03f4ae7f28d3f6ba82aed65cc59e
Sunil Kumar <sunil.kumar3@telekom-digital.com>
2026-06-02 20:14:47 +0530

    created Frist AI agent

--- Langchain/MyFirstAgent.py
+++ Langchain/MyFirstAgent.py
@@ -1,8 +1,18 @@
 from decouple import config
-print("Hello add my first AI agent")
+
+import os
+
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 GEMINI_API_KEY = config('GEMINI_API_KEY')
 OPEN_API_KEY = config('OPEN_API_KEY')
 
-print(GEMINI_API_KEY)
-print(OPEN_API_KEY)
+
+os.environ["GOOGLE_API_KEY"] = GEMINI_API_KEY
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+
+response = model.invoke("Who is prime minister of India?")
+
+
+print(response)
