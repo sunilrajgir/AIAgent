7efc06908d2aff7ebd226b77f0d22a1a8042d96f
Sunil Kumar <sunil.kumar3@telekom-digital.com>
2026-06-02 19:20:09 +0530

    changes

--- Langchain/MyFirstAgent.py
+++ Langchain/MyFirstAgent.py
@@ -1,2 +1,8 @@
 from decouple import config
 print("Hello add my first AI agent")
+
+GEMINI_API_KEY = config('GEMINI_API_KEY')
+OPEN_API_KEY = config('OPEN_API_KEY')
+
+print(GEMINI_API_KEY)
+print(OPEN_API_KEY)
